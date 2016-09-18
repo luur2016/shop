513b86cff8ee57562b58af824271a8d7af6db9a1
wangkai <wangkai@zy.com>
2016-09-18 21:20:29 +0800

    图片上传修改

--- src/main/java/com/shop/controller/admin/SiteConfigController.java
+++ src/main/java/com/shop/controller/admin/SiteConfigController.java
@@ -1,7 +1,5 @@
 package com.shop.controller.admin;
 
-import java.nio.file.Paths;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,166 +16,213 @@ import com.shop.tools.FileUpload;
 @Controller
 @RequestMapping("admin/config")
 public class SiteConfigController {
-	
-	@Autowired
-	SiteConfigService siteConfigService;
 
-	/**
-	 * 站点设置
-	 * @return
-	 */
-	@RequestMapping(value = "/siteconfig" , method = RequestMethod.GET)
-	public String index(Model model){
-		SettingDTO settings = siteConfigService.getAllSiteConfigs();
-		model.addAttribute("settings", settings);
-		return "admin/config/site_config";
-	}
-	
-	@RequestMapping(value = "/siteconfig" , method = RequestMethod.POST)
-	public String saveSiteConfig(
-			SettingDTO settingDto,MultipartFile site_logo_file,MultipartFile seller_center_logo_file,
-			MultipartFile site_mobile_logo_file,MultipartFile site_logowx_file,MultipartFile member_logo_file,
-			HttpServletRequest request){
-		String path=request.getSession().getServletContext().getRealPath("/data/upload/");
-		if(!site_logo_file.isEmpty()){
-			FileUpload.upload(site_logo_file, 
-					Paths.get(path, site_logo_file.getOriginalFilename()).toString());
-		}
- 		return null;
-	}
-	
-	/**
-	 * 账号同步
-	 * @return
-	 */
-	@RequestMapping("/accountsynchronization")
-	public String accountSynchronization(){
-		return "admin/config/account_synchronization";
-	}
-	
-	/**
-	 * 上传设置
-	 * @return
-	 */
-	@RequestMapping("/uploadconfig")
-	public String uploadConfig(){
-		return "admin/config/upload_config";
-	}
-	
-	/**
-	 * SEO设置
-	 * @return
-	 */
-	@RequestMapping("/seoconfig")
-	public String seoConfig(){
-		return "admin/config/seo_config";
-	}
-	
-	/**
-	 * 邮箱、短信设置
-	 * @return
-	 */
-	@RequestMapping("/mailandsms")
-	public String mailAndSmsConfig(){
-		return "admin/config/mail_sms_config";
-	}
-	
-	/**
-	 * 支付设置
-	 * @return
-	 */
-	@RequestMapping("/payconfig")
-	public String payConfig(){
-		return "admin/config/pay_config";
-	}
-	
-	/**
-	 * 权限设置
-	 * @return
-	 */
-	@RequestMapping("/authoritymanage")
-	public String authorityManage(){
-		return "admin/config/authority_manage";
-	}
-	
-	/**
-	 * 快递公司
-	 * @return
-	 */
-	@RequestMapping("/expressconfig")
-	public String expressConfig(){
-		return "admin/config/express_config";
-	}
-	
-	/**
-	 * 运单模板
-	 * @return
-	 */
-	@RequestMapping("/expressbillmodelconfig")
-	public String expressBillModelConfig(){
-		return "admin/config/expressbill_model_config";
-	}
-	
-	/**
-	 * 地区管理
-	 * @return
-	 */
-	@RequestMapping("/areaconfig")
-	public String areaConfig(){
-		return "admin/config/area_config";
-	}
-	
-	/**
-	 * 配送地区
-	 * @return
-	 */
-	@RequestMapping("/deliveryareaconfig")
-	public String deliveryAreaConfig(){
-		return "admin/config/deliveryarea_config";
-	}
-	
-	/**
-	 * 清除缓存
-	 * @return
-	 */
-	@RequestMapping("/cleancache")
-	public String cleanCache(){
-		return "admin/config/cleancache";
-	}
-	
-	/**
-	 * 数据备份
-	 * @return
-	 */
-	@RequestMapping("/databasebackup")
-	public String dbBackup(){
-		return "admin/config/database_backup";
-	}
-	
-	/**
-	 * 性能优化
-	 * @return
-	 */
-	@RequestMapping("/performanceoptimization")
-	public String performanceOptimization(){
-		return "admin/config/performance_optimization";
-	}
-	
-	/**
-	 * 搜索设置
-	 * @return
-	 */
-	@RequestMapping("/searchconfig")
-	public String searchConfig(){
-		return "admin/config/search_config";
-	}
-	
-	/**
-	 * 操作日志
-	 * @return
-	 */
-	@RequestMapping("/operationlog")
-	public String operationLog(){
-		return "admin/config/operation_log";
-	}
+  @Autowired
+  SiteConfigService siteConfigService;
+
+  /**
+   * 站点设置
+   * 
+   * @return
+   */
+  @RequestMapping(value = "/siteconfig", method = RequestMethod.GET)
+  public String index(Model model) {
+    SettingDTO settings = siteConfigService.getAllSiteConfigs();
+    model.addAttribute("settings", settings);
+    return "admin/config/site_config";
+  }
+
+  @RequestMapping(value = "/siteconfig", method = RequestMethod.POST)
+  public String saveSiteConfig(SettingDTO settingDto, MultipartFile site_logo_file,
+      MultipartFile seller_center_logo_file, MultipartFile site_mobile_logo_file,
+      MultipartFile site_logowx_file, MultipartFile member_logo_file, HttpServletRequest request) {
+    
+    String path = request.getSession().getServletContext().getRealPath("/");
+    String uploadPath = path + "/data/upload/";
+    
+    // site_logo
+    if (!site_logo_file.isEmpty()) {
+      FileUpload.upload(site_logo_file, uploadPath);
+      settingDto.setSite_logo(site_logo_file.getOriginalFilename());
+    }
+    
+    // seller_center_logo_file
+    if (!seller_center_logo_file.isEmpty()) {
+      FileUpload.upload(seller_center_logo_file, uploadPath);
+      settingDto.setSeller_center_logo(seller_center_logo_file.getOriginalFilename());
+    }
+
+    // site_mobile_logo_file
+    if (!site_mobile_logo_file.isEmpty()) {
+      FileUpload.upload(site_mobile_logo_file, uploadPath);
+      settingDto.setSite_mobile_logo(site_mobile_logo_file.getOriginalFilename());
+    }
+
+    // site_logowx_file
+    if (!site_logowx_file.isEmpty()) {
+      FileUpload.upload(site_logowx_file, uploadPath);
+      settingDto.setSite_logowx(site_logowx_file.getOriginalFilename());
+    }
+
+    // member_logo_file
+    if (!member_logo_file.isEmpty()) {
+      FileUpload.upload(member_logo_file, uploadPath);
+      settingDto.setMember_logo(member_logo_file.getOriginalFilename());
+    }
+    
+    // save data
+    
+    
+    return "redirect:/siteconfig";
+  }
+
+  /**
+   * 账号同步
+   * 
+   * @return
+   */
+  @RequestMapping("/accountsynchronization")
+  public String accountSynchronization() {
+    return "admin/config/account_synchronization";
+  }
+
+  /**
+   * 上传设置
+   * 
+   * @return
+   */
+  @RequestMapping("/uploadconfig")
+  public String uploadConfig() {
+    return "admin/config/upload_config";
+  }
+
+  /**
+   * SEO设置
+   * 
+   * @return
+   */
+  @RequestMapping("/seoconfig")
+  public String seoConfig() {
+    return "admin/config/seo_config";
+  }
+
+  /**
+   * 邮箱、短信设置
+   * 
+   * @return
+   */
+  @RequestMapping("/mailandsms")
+  public String mailAndSmsConfig() {
+    return "admin/config/mail_sms_config";
+  }
+
+  /**
+   * 支付设置
+   * 
+   * @return
+   */
+  @RequestMapping("/payconfig")
+  public String payConfig() {
+    return "admin/config/pay_config";
+  }
+
+  /**
+   * 权限设置
+   * 
+   * @return
+   */
+  @RequestMapping("/authoritymanage")
+  public String authorityManage() {
+    return "admin/config/authority_manage";
+  }
+
+  /**
+   * 快递公司
+   * 
+   * @return
+   */
+  @RequestMapping("/expressconfig")
+  public String expressConfig() {
+    return "admin/config/express_config";
+  }
+
+  /**
+   * 运单模板
+   * 
+   * @return
+   */
+  @RequestMapping("/expressbillmodelconfig")
+  public String expressBillModelConfig() {
+    return "admin/config/expressbill_model_config";
+  }
+
+  /**
+   * 地区管理
+   * 
+   * @return
+   */
+  @RequestMapping("/areaconfig")
+  public String areaConfig() {
+    return "admin/config/area_config";
+  }
+
+  /**
+   * 配送地区
+   * 
+   * @return
+   */
+  @RequestMapping("/deliveryareaconfig")
+  public String deliveryAreaConfig() {
+    return "admin/config/deliveryarea_config";
+  }
+
+  /**
+   * 清除缓存
+   * 
+   * @return
+   */
+  @RequestMapping("/cleancache")
+  public String cleanCache() {
+    return "admin/config/cleancache";
+  }
+
+  /**
+   * 数据备份
+   * 
+   * @return
+   */
+  @RequestMapping("/databasebackup")
+  public String dbBackup() {
+    return "admin/config/database_backup";
+  }
+
+  /**
+   * 性能优化
+   * 
+   * @return
+   */
+  @RequestMapping("/performanceoptimization")
+  public String performanceOptimization() {
+    return "admin/config/performance_optimization";
+  }
+
+  /**
+   * 搜索设置
+   * 
+   * @return
+   */
+  @RequestMapping("/searchconfig")
+  public String searchConfig() {
+    return "admin/config/search_config";
+  }
+
+  /**
+   * 操作日志
+   * 
+   * @return
+   */
+  @RequestMapping("/operationlog")
+  public String operationLog() {
+    return "admin/config/operation_log";
+  }
 }
--- src/main/java/com/shop/service/admin/SiteConfigService.java
+++ src/main/java/com/shop/service/admin/SiteConfigService.java
@@ -1,5 +1,6 @@
 package com.shop.service.admin;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,50 +12,74 @@ import com.shop.model.admin.Setting;
 
 @Service
 public class SiteConfigService {
-	
-	@Autowired
-	SettingMapper settingMapper;
-	
-	/**
-	 * 获取所有设置
-	 * @return
-	 */
-	public SettingDTO getAllSiteConfigs(){
-		List<Setting> settings = settingMapper.selectAll();
-		SettingDTO settingDTO = new SettingDTO();
-		for(Setting setting : settings){
-			String name = setting.getName();
-			if("site_name".equals(name))
-				settingDTO.setSite_name(setting.getValue());
-			if("site_logo".equals(name))
-				settingDTO.setSite_logo(setting.getValue());
-			if("site_mobile_logo".equals(name))
-				settingDTO.setSite_mobile_logo(setting.getValue());
-			if("site_logowx".equals(name))
-				settingDTO.setSite_logowx(setting.getValue());
-			if("member_logo".equals(name))
-				settingDTO.setMember_logo(setting.getValue());
-			if("seller_center_logo".equals(name))
-				settingDTO.setSeller_center_logo(setting.getValue());
-			if("time_zone".equals(name))
-				settingDTO.setTime_zone(setting.getValue());
-			if("site_phone".equals(name))
-				settingDTO.setSite_phone(setting.getValue());
-			if("site_bank_account".equals(name))
-				settingDTO.setSite_bank_account(setting.getValue());
-			if("site_email".equals(name))
-				settingDTO.setSite_email(setting.getValue());
-			if("statistics_code".equals(name))
-				settingDTO.setStatistics_code(setting.getValue());
-			if("icp_number".equals(name))
-				settingDTO.setIcp_number(setting.getValue());
-			if("site_tel400".equals(name))
-				settingDTO.setSite_tel400(setting.getValue());
-			if("site_status".equals(name))
-				settingDTO.setSite_status(setting.getValue());
-			if("closed_reason".equals(name))
-				settingDTO.setClosed_reason(setting.getValue());
-		}
-		return settingDTO;
-	}
+
+  @Autowired
+  SettingMapper settingMapper;
+
+  /**
+   * 获取所有设置
+   * 
+   * @return
+   */
+  public SettingDTO getAllSiteConfigs() {
+    List<Setting> settings = settingMapper.selectAll();
+    SettingDTO settingDTO = new SettingDTO();
+    for (Setting setting : settings) {
+      String name = setting.getName();
+      if ("site_name".equals(name))
+        settingDTO.setSite_name(setting.getValue());
+      if ("site_logo".equals(name))
+        settingDTO.setSite_logo(setting.getValue());
+      if ("site_mobile_logo".equals(name))
+        settingDTO.setSite_mobile_logo(setting.getValue());
+      if ("site_logowx".equals(name))
+        settingDTO.setSite_logowx(setting.getValue());
+      if ("member_logo".equals(name))
+        settingDTO.setMember_logo(setting.getValue());
+      if ("seller_center_logo".equals(name))
+        settingDTO.setSeller_center_logo(setting.getValue());
+      if ("time_zone".equals(name))
+        settingDTO.setTime_zone(setting.getValue());
+      if ("site_phone".equals(name))
+        settingDTO.setSite_phone(setting.getValue());
+      if ("site_bank_account".equals(name))
+        settingDTO.setSite_bank_account(setting.getValue());
+      if ("site_email".equals(name))
+        settingDTO.setSite_email(setting.getValue());
+      if ("statistics_code".equals(name))
+        settingDTO.setStatistics_code(setting.getValue());
+      if ("icp_number".equals(name))
+        settingDTO.setIcp_number(setting.getValue());
+      if ("site_tel400".equals(name))
+        settingDTO.setSite_tel400(setting.getValue());
+      if ("site_status".equals(name))
+        settingDTO.setSite_status(setting.getValue());
+      if ("closed_reason".equals(name))
+        settingDTO.setClosed_reason(setting.getValue());
+    }
+    return settingDTO;
+  }
+
+  public boolean addSiteConfigs(SettingDTO settingDto){
+    
+    List<Setting> settings = new ArrayList<Setting>();
+    
+    Setting site_name = Setting.builder().name("site_name").value(settingDto.getSite_name()).build();
+    Setting site_logo = Setting.builder().name("site_logo").value(settingDto.getSite_logo()).build();
+    Setting site_mobile_logo = Setting.builder().name("site_mobile_logo").value(settingDto.getSite_mobile_logo()).build();
+    Setting site_logowx = Setting.builder().name("site_logowx").value(settingDto.getSite_logowx()).build();
+    Setting member_logo = Setting.builder().name("member_logo").value(settingDto.getMember_logo()).build();
+    Setting time_zone = Setting.builder().name("time_zone").value(settingDto.getTime_zone()).build();
+    Setting site_phone = Setting.builder().name("site_phone").value(settingDto.getSite_phone()).build();
+    Setting site_bank_account = Setting.builder().name("site_bank_account").value(settingDto.getSite_bank_account()).build();
+    Setting site_email = Setting.builder().name("site_email").value(settingDto.getSite_email()).build();
+    Setting statistics_code = Setting.builder().name("statistics_code").value(settingDto.getStatistics_code()).build();
+    Setting icp_number = Setting.builder().name("icp_number").value(settingDto.getIcp_number()).build();
+    Setting site_tel400 = Setting.builder().name("site_tel400").value(settingDto.getSite_tel400()).build();
+    Setting site_status = Setting.builder().name("site_status").value(settingDto.getSite_status()).build();
+    Setting closed_reason = Setting.builder().name("closed_reason").value(settingDto.getClosed_reason()).build();
+    
+    
+    return false;
+  }
 }
